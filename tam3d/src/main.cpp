69e0048dbd3c0494e89a6eb24b7748f2e8ed7b5c
Felipe Ricobello <64718731+felpsrr3@users.noreply.github.com>
2021-02-21 01:12:05 -0300

    Messages/WndProc

--- tam3d/src/main.cpp
+++ tam3d/src/main.cpp
@@ -1,5 +1,18 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, 
+						 UINT msg, 
+						 WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(2);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -11,22 +24,22 @@ int CALLBACK WinMain(
 	/*WINDOW REGISTER*/
 	WNDCLASSEX wc = {0};
 
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
+		wc.cbSize = sizeof(wc);
+		wc.style = CS_OWNDC;
 
-	wc.lpfnWndProc = DefWindowProc;
+		wc.lpfnWndProc = WndProc;
 
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
 
-	wc.hInstance = hInstance;
-	wc.hIcon = NULL;
-	wc.hIconSm = NULL;
-	wc.hCursor = NULL;
-	wc.hbrBackground = NULL;
+		wc.hInstance = hInstance;
+		wc.hIcon = NULL;
+		wc.hIconSm = NULL;
+		wc.hCursor = NULL;
+		wc.hbrBackground = NULL;
 
-	wc.lpszClassName = pClassName;
-	wc.lpszMenuName = NULL;
+		wc.lpszClassName = pClassName;
+		wc.lpszMenuName = NULL;
 
 	RegisterClassEx(&wc);
 
@@ -39,6 +52,25 @@ int CALLBACK WinMain(
 	);
 
 	ShowWindow(hWnd, SW_SHOW);
-	while (1);
+	
+	/*MESSAGES*/
+	MSG msg;
+	BOOL gResult;
+
+	while ((gResult = GetMessage(&msg, NULL, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 	return 0;
 }
