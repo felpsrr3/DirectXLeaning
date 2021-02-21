775e5f5d9547c165ace6f5365c2c3bd97fb31ac9
Felipe Ricobello <64718731+felpsrr3@users.noreply.github.com>
2021-02-21 00:16:59 -0300

    Add files via upload

--- tam3d/src/main.cpp
+++ tam3d/src/main.cpp
@@ -6,5 +6,39 @@ int CALLBACK WinMain(
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {
+	const auto pClassName = "tam3d";
+
+	/*WINDOW REGISTER*/
+	WNDCLASSEX wc = {0};
+
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+
+	wc.lpfnWndProc = DefWindowProc;
+
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+
+	wc.hInstance = hInstance;
+	wc.hIcon = NULL;
+	wc.hIconSm = NULL;
+	wc.hCursor = NULL;
+	wc.hbrBackground = NULL;
+
+	wc.lpszClassName = pClassName;
+	wc.lpszMenuName = NULL;
+
+	RegisterClassEx(&wc);
+
+	/*CREATING WINDOW*/
+	HWND hWnd = CreateWindowEx(
+		0, pClassName, "Eventually",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		NULL, NULL, hInstance, NULL
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+	while (1);
 	return 0;
 }
